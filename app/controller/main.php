71a4efc5e877ad6147ff248f68495bca222d7807
Vitaly Iegorov <egorov@samsonos.com>
2014-05-23 13:04:38 +0300

    Initialization

---
+++ app/controller/main.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 23.05.14 at 13:01
+ */
+
+function main__HANDLER()
+{
+    m()->view('main')->title('Main page');
+}
---
+++ app/view/main.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>
+ * on 23.05.14 at 13:02
+ */
+?>
+
+<h1>This is main page view</h1>
+  
