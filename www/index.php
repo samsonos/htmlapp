3b188351c7a07d18f2083ce3727ac8f87752fa88
Vitaly Iegorov <egorov@samsonos.com>
2014-08-05 12:42:29 +0300

    Added todo

--- www/index.php
+++ www/index.php
@@ -1,6 +1,8 @@
 <?php
 // Configuration
-require( 'config.php' );
+require('config.php');
+
+// TODO: add validator of default files to force user to change them
 
 // Load SamsonPHP core
 s()->composer()->start('main');
