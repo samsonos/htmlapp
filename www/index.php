49d9f5123c7daf799f10a97c3b5515e52b93da20
Vitaly Iegorov <egorov@samsonos.com>
2014-09-08 09:55:29 +0300

    Added e404 handler Removed localization commands

--- www/index.php
+++ www/index.php
@@ -1,17 +1,11 @@
 <?php
 /** SamsonPHP init script */
 
-/** Set default locale to - English! */
-define('DEFAULT_LOCALE', 'en');
-
 /** Set new project structure vendor path */
 define('__SAMSON_VENDOR_PATH', '../vendor/');
 
 /** Load SamsonPHP framework */
 require(__SAMSON_VENDOR_PATH.'/autoload.php');
 
-/** Set supported locales */
-setlocales('ru', 'ua', 'en');
-
 /** Start SamsonPHP web-application */
-s()->composer()->e404('e404')->start( 'main' );
+s()->composer()->subscribe('core.e404', 'main_e404')->start( 'main' );
