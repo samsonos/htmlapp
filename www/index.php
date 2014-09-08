e114c259e75332f8b643be2d18f76de02eb84580
Vitaly Iegorov <egorov@samsonos.com>
2014-09-08 09:56:24 +0300

    Added localization commands

--- www/index.php
+++ www/index.php
@@ -1,11 +1,17 @@
 <?php
 /** SamsonPHP init script */
 
+/** Set default locale to - Russian */
+define('DEFAULT_LOCALE', 'ru');
+
 /** Set new project structure vendor path */
 define('__SAMSON_VENDOR_PATH', '../vendor/');
 
 /** Load SamsonPHP framework */
 require(__SAMSON_VENDOR_PATH.'/autoload.php');
+
+/** Set supported locales */
+setlocales('ru');
 
 /** Start SamsonPHP web-application */
 s()->composer()->subscribe('core.e404', 'main_e404')->start( 'main' );
