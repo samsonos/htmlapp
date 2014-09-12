eaef3e6cadb723cd0b773302c9bed90bdbc6fa78
Vitaly Iegorov <egorov@samsonos.com>
2014-09-12 14:59:17 +0300

    PHPCS fixes

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -1,4 +1,5 @@
 <?php
+namespace tests;
 /**
  * Created by Vitaly Iegorov <egorov@samsonos.com>
  * on 04.08.14 at 16:42
@@ -14,7 +15,7 @@ class CMSMainTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('foo', $stack[count($stack)-1]);
         $this->assertEquals(1, count($stack));
 
-		// Change to make push
+        // Change to make push
         $this->assertEquals('foo', array_pop($stack));
         $this->assertEquals(0, count($stack));
     }
